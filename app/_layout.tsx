7262b1f974d0a2037cfaa786515ac9ba6afab895
kid <kidiatoliny@akira-io.com>
2024-12-26 00:35:01 -0100

    feat: add sign-in screen and configure environment variables
    Introduced a new "SignIn" component integrated into the app entry point. Added environment variable configuration for Appwrite integration. Commented out the global provider in the layout file as it's currently unused.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import {useFonts} from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
 import "./global.css";
-import GlobalProvider from "@/libs/global-provider";
 
 export default function RootLayout() {
     const [fontsLoaded] = useFonts({
@@ -27,8 +26,8 @@ export default function RootLayout() {
     }
 
     return (
-        <GlobalProvider>
-            <Stack screenOptions={{headerShown: false}}/>
-        </GlobalProvider>
+        // <GlobalProvider>
+        <Stack screenOptions={{headerShown: false}}/>
+        // </GlobalProvider>
     );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import { Text, View } from "react-native";
+import {View} from "react-native";
+import SignIn from "@/app/sign-in";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <SignIn/>
+        </View>
+    );
 }
